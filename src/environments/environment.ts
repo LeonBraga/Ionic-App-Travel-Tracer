75eafe15c356e515c16ea0f9339d3c91646eabcb
UsuarioTeste <leonathanbraga@id.uff.br>
2019-11-02 18:39:58 -0300

    css e fontes de login e grupos incluidos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-/* export const environment = {
+ export const environment = {
   production: false,
   firebase: {
     apiKey: "AIzaSyBQljMFx0tyb-JIN4E2qKmrBiYvieHXgn8",
@@ -13,20 +13,20 @@
     messagingSenderId: "394606031864",
     appId: "1:394606031864:web:5e1a48c8ae66b9e52d9c0d"
   }
-}; */
-
-export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyDfjYZ2amT6jSsSnK20nmXiDHjEHn4PtB4",
-    authDomain: "traveltracer-55bde.firebaseapp.com",
-    databaseURL: "https://traveltracer-55bde.firebaseio.com",
-    projectId: "traveltracer-55bde",
-    storageBucket: "traveltracer-55bde.appspot.com",
-    messagingSenderId: "758912421385",
-    appId: "1:758912421385:web:955f4d12af1ceb3f1232d8"
-  }
 };
+
+// export const environment = {
+//   production: false,
+//   firebase: {
+//     apiKey: "AIzaSyDfjYZ2amT6jSsSnK20nmXiDHjEHn4PtB4",
+//     authDomain: "traveltracer-55bde.firebaseapp.com",
+//     databaseURL: "https://traveltracer-55bde.firebaseio.com",
+//     projectId: "traveltracer-55bde",
+//     storageBucket: "traveltracer-55bde.appspot.com",
+//     messagingSenderId: "758912421385",
+//     appId: "1:758912421385:web:955f4d12af1ceb3f1232d8"
+//   }
+// };
 
 
 
